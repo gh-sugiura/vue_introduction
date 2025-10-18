dd6ab978b3d503c1cc60ad3da7972179281a55e3
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-19 01:50:57 +0900

    v-modelの練習

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -2,80 +2,101 @@
 import { ref } from 'vue'
 
 
-const randValue = ref("まだです")
-const onButtonClick = (): void => {
-	const rand = Math.round(Math.random() * 10);
-	randValue.value = String(rand);
-}
+const inputNameModel = ref("双方向バインディング");
 
 
-const mousePointerX = ref(0);
-const mousePointerY = ref(0);
-const onImgMouseMove = (event: MouseEvent): void => {
-	mousePointerX.value = event.offsetX;
-	mousePointerY.value = event.offsetY;
+const inputNameBind = ref("a");
+const inputNameOn = ref("b");
+const onInputName = (event: Event): void => {
+	const element = event.target as HTMLInputElement;
+	inputNameOn.value = element.value;
 };
 
 
-const pBgColor = ref("blue");
-const onPClick = (bgColor: string): void => {
-	pBgColor.value = bgColor;
+const inputName2Way = ref("2wayバインディング");
+const onInputName2Way = (event: Event): void => {
+	const element = event.target as HTMLInputElement;
+	inputName2Way.value = element.value;
 };
 
 
-const pMsg = ref("イベント前");
-const pBgColorEvent = ref("green");
-const onPClickWithEvent = (bgColor: string, event: MouseEvent): void => {
-	pBgColorEvent.value = bgColor;
-	pMsg.value = event.timeStamp.toString();
-};
-
-
-const msg = ref("まだです。");
-const onEntryKey = (): void => {
-	msg.value = "エンターキーが押されました。";
-};
-const onRightButtonCkick = (): void => {
-	msg.value = "右クリックされました。";
-};
-function onShiftClick(): void {
-	msg.value = "Shiftキーを押しながらクリックされました。";
-}
+const inputTextarea = ref("テキストエリアへの入力文字。\n改行も加えてみる。");
+const memberType = ref(1);
+const memberTypeSelect = ref(1);
+const isAgreed = ref(false);
+const isAgreed01 = ref(0);
+const selectedOS = ref([]);
+const selectedOSSelect = ref([]);
 </script>
 
 
 <template>
 	<section>
-		<button v-on:click="onButtonClick">クリック</button>
-		<p>result:{{ randValue }}</p>
+		<input type="text" v-model="inputNameModel" />
+		<p>入力: {{ inputNameModel }}</p>
 	</section>
+
 
 	<section>
-		<img
-			v-on:mousemove="onImgMouseMove"
-			src="./assets/logo.svg"
-			alt="Vue logo"
-			width="200px"
-		/>
-		<p>マウスポインタの座標: X={{ mousePointerX }} , Y={{ mousePointerY }}</p>
+		<input type="text" v-bind:value="inputNameBind" />
+	</section>
+	<br>
+	<section>
+		<input type="text" v-on:input="onInputName" />
+		<p>入力: {{ inputNameOn }}</p>
 	</section>
 
-	<p 
-		v-on:click="onPClick('red')"
-		v-bind:style="{ backgroundColor: pBgColor }"
-	>
-		クリックすると背景色が変わります
-	</p>
 
-	<p 
-		v-on:click="onPClickWithEvent('red', $event)"
-		v-bind:style="{ backgroundColor: pBgColorEvent }"
-	>
-		{{ pMsg }}
-	</p>
+	<section>
+		<input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way"/>
+		<p>入力: {{ inputName2Way }}</p>
+	</section>
+	
 
-	<p>{{ msg }}</p>
-	<input type="text" v-on:keydown.enter="onEntryKey"/>
-	<button v-on:click.right="onRightButtonCkick">右クリック</button>
-	<button v-on:click.shift="onShiftClick">Shiftキーを押しながらクリック</button>
+	<textarea v-model="inputTextarea"></textarea>
+	<br>
+	<section>
+		<label><input type="radio" name="memberType" value="1" v-model="memberType" >通常会員</label>
+		<label><input type="radio" name="memberType" value="2" v-model="memberType" >特別会員</label>
+		<label><input type="radio" name="memberType" value="3" v-model="memberType" >優良会員</label>
+		<br>
+		<p>選択されたラジオボタン: {{memberType}}</p>
+	</section>
+	<br>
+	<section>
+		<select v-model="memberTypeSelect">
+			<option value="1">通常会員</option>
+			<option value="2">特別会員</option>
+			<option value="3">優良会員</option>
+		</select>
+		<br>
+		<p>選択されたリスト: {{memberTypeSelect}}</p>
+	</section>
+	<br>
+	<section>
+		<label><input type="checkbox" v-model="isAgreed">同意する</label>
+		<p>同意の結果: {{isAgreed}}</p>
+	</section>
+	<section>
+		<label><input type="checkbox" v-model="isAgreed01" true-value="1" false-value="0">同意する</label>
+		<p>同意の結果: {{isAgreed01}}</p>
+	</section>
+	<section>
+		<label><input type="checkbox" v-model="selectedOS" value="1">macOS</label>
+		<label><input type="checkbox" v-model="selectedOS" value="2">Windows</label>
+		<label><input type="checkbox" v-model="selectedOS" value="3">Linux</label>
+		<label><input type="checkbox" v-model="selectedOS" value="4">iOS</label>
+		<label><input type="checkbox" v-model="selectedOS" value="5">Android</label>
+		<p>選択されたOS: {{selectedOS}}</p>
+	</section>
+	<section>
+		<select v-model="selectedOSSelect" multiple>
+			<option value="1">macOS</option>
+			<option value="2">Windows</option>
+			<option value="3">Linux</option>
+			<option value="4">iOS</option>
+			<option value="5">Android</option>
+		</select>
+		<p>選択されたOS: {{selectedOSSelect}}</p>
+	</section>
 </template>
