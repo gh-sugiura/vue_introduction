377157c6780c1a143fa90ed0f7f8a98f10e8a4c9
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-20 00:27:57 +0900

    lifecycle-hookの練習

---
+++ frontend/vue-project/src/old_app/App_feature_watcher.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { ref, watchEffect, watch } from 'vue'
+
+
+const cocktailNo = ref(1);
+const priceMsg = ref("");
+// watchEffect(
+// 	(): void => {
+// 		priceMsg.value = getCocktailInfo(cocktailNo.value);
+// 	}
+// );
+
+
+// watch(cocktailNo,
+// 	(): void => {
+// 		priceMsg.value = getCocktailInfo(cocktailNo.value);
+// 	},
+// 	{ immediate: true }
+// );
+
+
+// 24行目がエラーのためコメントアウト。ブラウザで動作はする。
+// watch(cocktailNo,
+// 	(newVal: number, oldVal: number): void => {
+// 		let msg = "前のカクテル：";
+// 		msg += getCocktailInfo(oldVal);
+// 		msg += "現在のカクテル：";
+// 		msg += getCocktailInfo(newVal);
+// 		priceMsg.value = msg;
+// 	},
+// 	{ immediate: true }
+// );
+
+
+// ChatGPTによる修正案
+// 40行目にエラーあり。おそらく、初回の実行ではoldValが存在しないためundefinedになる。
+// watch(
+//   (): number => cocktailNo.value,
+//   (newVal, oldVal) => {
+//     let msg = "前のカクテル：";
+//     msg += getCocktailInfo(oldVal);
+//     msg += "現在のカクテル：";
+//     msg += getCocktailInfo(newVal);
+//     priceMsg.value = msg;
+//   },
+//   { immediate: true }
+// );
+
+
+
+function getCocktailInfo(cocktailNo: number): string {
+	const cocktailDataListsInit = new Map<number, Cocktail>();
+	cocktailDataListsInit.set(1, {id: 1, name: "ホワイトレディ", price: 1200});
+	cocktailDataListsInit.set(2, {id: 2, name: "ブルーハワイ", price: 1500});
+	cocktailDataListsInit.set(3, {id: 3, name: "ニューヨーク", price: 1100});
+	cocktailDataListsInit.set(4, { id: 4, name: "マティーニ", price: 1500 });
+
+	const cocktail = cocktailDataListsInit.get(cocktailNo);
+	let msg = "該当するカクテルはありません。";
+	if (cocktail !== undefined) {
+		msg = `カクテル：${cocktail.name}、価格：${cocktail.price}円`;
+	}
+	return msg;
+}
+
+
+interface Cocktail {
+	id: number;
+	name: string;
+	price: number;
+}
+
+
+setInterval(
+	(): void => {
+		cocktailNo.value = Math.floor(Math.random() * 4) + 1;
+	},
+	1000
+);
+</script>
+
+
+<template>
+	<p>Cocktail No:{{ cocktailNo }}</p>
+	<p>{{ priceMsg }}</p>
+</template>
