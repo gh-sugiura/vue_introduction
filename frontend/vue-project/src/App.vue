bc3c1cd813d1578ed5c59008187dd16c9d6da50e
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-19 11:36:17 +0900

    ループの練習

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,44 +1,116 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref } from 'vue'
 
 
-const randomNumber = computed(
-	(): number => {
-		Math.random()
-		return Math.round(Math.random() * 100);
-	}
-);
+const cocktailListsInt: string[] = [
+	'Mojito',
+	'Martini',
+	'Margarita',
+	'Old Fashioned',
+	'Daiquiri',
+	'Negroni',
+	'Cosmopolitan',
+	'Pina Colada',
+	'Bloody Mary',
+	'Whiskey Sour'
+];
+const cocktailLists = ref(cocktailListsInt);
 
 
-const showOrNot = computed(
-	(): boolean => {
-		let showOrNot = false;
-		const rand = Math.round(Math.random() * 100);
-		if(rand >= 50) {
-			showOrNot = true;
-		}
-		return showOrNot;
-	}
-);
+const cocktailListsInit2: {[key: number]: string;} = {
+	2345: "ホワイトレディ",
+	4412: "ブルーハワイ",
+	6792: "ニューヨーク"
+};
+const cocktailLists2 = ref(cocktailListsInit2);
+
+
+const cocktailListsInit3 = new Map<number, string>();
+cocktailListsInit3.set(2345, "ホワイトレディ");
+cocktailListsInit3.set(4412, "ブルーハワイ");
+cocktailListsInit3.set(6792, "ニューヨーク");
+const cocktailLists3 = ref(cocktailListsInit3);
+
+
+const whiteLadyInit: {
+	id: number;
+	name: string;
+	price: number;
+	recipe: string;
+} = {
+	id: 2345,
+	name: "ホワイトレディ",
+	price: 1200,
+	recipe: "ジン30ml+コワントロー15ml+レモン果汁+15ml"
+};
+const whiteLady = ref(whiteLadyInit);
+
+
+const cocktailDataListsInit: Cocktail[]  = [
+	{id: 2345, name: "ホワイトレディ", price: 1200},
+	{id: 4412, name: "ブルーハワイ", price: 1500},
+	{id: 6792, name: "ニューヨーク", price: 1100},
+	{id: 8429, name: "マティーニ", price: 1500},
+];
+const cocktailDataLists = ref(cocktailDataListsInit);
+
+interface Cocktail {
+	id: number;
+	name: string;
+	price: number;
+}
 </script>
 
 
 <template>
-	<p>
-		得点: {{ randomNumber }}
-		<span v-if="randomNumber >= 80">優</span>
-		<span v-else-if="randomNumber >= 70">良</span>
-		<span v-else-if="randomNumber >= 60">可</span>
-		<span v-else>不可</span>
-	</p>
+	<ul>
+		<li v-for="cocktailList in cocktailLists" v-bind:key="cocktailList">
+			{{ cocktailList }}(index:{{ cocktailList }})
+		</li>
+	</ul>
+	<ul>
+		<li v-for="(cocktailList, index) in cocktailLists" v-bind:key="index">
+			{{ cocktailList }}(index:{{ index }})
+		</li>
+	</ul>
 
-	
-	<section>
-		v-if
-		<p v-if="showOrNot">表示されました。</p>
-	</section>
-	<section>
-		v-show
-		<p v-show="showOrNot">表示されました。</p>
-	</section>
+
+	<ul>
+		<li v-for="(cocktailList2, id) in cocktailLists2" v-bind:key="'cocktailList2' + id">
+			ID:{{ id }}, Cocktail:{{ cocktailList2 }}
+		</li>
+	</ul>
+	<ul>
+		<li v-for="(cocktailList2, id, index) in cocktailLists2" v-bind:key="'cocktailList2WithIndex' + id">
+			{{ index + 1 }}->ID:{{ id }}, Cocktail:{{ cocktailList2 }}
+		</li>
+	</ul>
+
+
+	<ul>
+		<li v-for="[id, cocktailList3] in cocktailLists3" v-bind:key="id">
+			ID:{{ id }}, Cocktail:{{ cocktailList3 }}
+		</li>
+	</ul>
+
+
+	<dl>
+		<template v-for="(value, key) in whiteLady" v-bind:key="key">
+			<dt>{{ key }}</dt>
+			<dd>{{ value }}</dd>
+		</template>
+	</dl>
+
+
+	<ul>
+		<li v-for="cocktailDataList in cocktailDataLists" v-bind:key="cocktailDataList.id">
+			ID:{{ cocktailDataList.id }}, Name:{{ cocktailDataList.name }}, Price:{{ cocktailDataList.price }}円
+		</li>
+	</ul>
+
+	<select>
+		<option v-for="year in 60" v-bind:key="year" v-bind:value="year + 1965">
+			{{ year + 1965 }}
+		</option>
+	</select>
 </template>
---
+++ frontend/vue-project/src/old_app/App_feature_v-if.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+
+const randomNumber = computed(
+	(): number => {
+		Math.random()
+		return Math.round(Math.random() * 100);
+	}
+);
+
+
+const showOrNot = computed(
+	(): boolean => {
+		let showOrNot = false;
+		const rand = Math.round(Math.random() * 100);
+		if(rand >= 50) {
+			showOrNot = true;
+		}
+		return showOrNot;
+	}
+);
+</script>
+
+
+<template>
+	<p>
+		得点: {{ randomNumber }}
+		<span v-if="randomNumber >= 80">優</span>
+		<span v-else-if="randomNumber >= 70">良</span>
+		<span v-else-if="randomNumber >= 60">可</span>
+		<span v-else>不可</span>
+	</p>
+
+	
+	<section>
+		v-if
+		<p v-if="showOrNot">表示されました。</p>
+	</section>
+	<section>
+		v-show
+		<p v-show="showOrNot">表示されました。</p>
+	</section>
+</template>
