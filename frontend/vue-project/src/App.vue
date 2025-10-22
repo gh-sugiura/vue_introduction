c0cf049f639f79912e05fb3195e1c831686cd681
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-22 16:35:52 +0900

    App.vueno
    App.vueのreactiveの位置を変更

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -4,12 +4,14 @@ import BaseSection from './components/BaseSection.vue';
 import type { Member } from './stores/interfaces';
 
 
-const memberLists = new Map<number, Member>([
-	[1, { id: 1, name: "田中", email: "tanaka@example.com", points: 35, note: "初回特典あり" }],
-	[2, { id: 2, name: "鈴木", email: "suzuki@example.com", points: 53 }],
-	[3, { id: 3, name: "伊藤", email: "ito@example.com", points: 80 }],
-]);
-provide("memberLists", reactive(memberLists));
+const memberLists = reactive(
+	new Map<number, Member>([
+		[1, { id: 1, name: "田中", email: "tanaka@example.com", points: 35, note: "初回特典あり" }],
+		[2, { id: 2, name: "鈴木", email: "suzuki@example.com", points: 53 }],
+		[3, { id: 3, name: "伊藤", email: "ito@example.com", points: 80 }],
+	])
+);
+provide("memberLists", memberLists);
 </script>
 
 
