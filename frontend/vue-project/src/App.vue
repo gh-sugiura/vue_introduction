0eee1bf3024ee02b9dfe2f234621185a641f2318
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-23 13:44:43 +0900

    routerの基本

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,40 +1,52 @@
 <script setup lang="ts">
-import {ref} from "vue";
-import Input from "./components/Input.vue";
-import Radio from "./components/Radio.vue";
-import Select from "./components/Select.vue";
+import { reactive, provide } from "vue";
+import {RouterView} from "vue-router";
+import type { Member } from '@/stores/interfaces';
 
 
-const currentComponent = ref(Input);
-const currentComponentName = ref("Input");
-const componentList = [
-	Input,
-	Radio,
-	Select,
-];
-const componentNameList: string[] = [
-	"Input",
-	"Radio",
-	"Select",
-];
-
-
-let currentComponentIndex = 0;
-const switchComponent = (): void => {
-	currentComponentIndex++;
-	if (currentComponentIndex >= componentList.length) {
-		currentComponentIndex = 0;
-	}
-	currentComponent.value = componentList[currentComponentIndex]!;
-	currentComponentName.value = componentNameList[currentComponentIndex]!;
-};
+const memberLists = reactive(
+	new Map<number, Member>([
+		[1, { id: 1, name: "田中", email: "tanaka@example.com", points: 35, note: "初回特典あり" }],
+		[2, { id: 2, name: "鈴木", email: "suzuki@example.com", points: 53 }],
+		[3, { id: 3, name: "伊藤", email: "ito@example.com", points: 80 }],
+	])
+);
+provide("memberLists", memberLists);
 </script>
 
 
 <template>
-	<p>コンポーネント名：{{ currentComponentName }}</p>
-	<keep-alive>
-		<component v-bind:is="currentComponent" />
-	</keep-alive>
-	<button v-on:click="switchComponent">コンポーネント切り替え</button>
+	<header>
+		<h1>Vue Router サンプル</h1>
+	</header>
+	<main>
+		<RouterView />
+	</main>
 </template>
+
+
+<style scoped>
+main {
+	border: blue 1px solid;
+	padding: 10px;
+}
+#breadcrumbs ul li {
+	display: inline;
+	list-style-type: none;
+}
+#breadcrumbs {
+	margin-left: 0px;
+}
+#breadcrumbs ul {
+	padding-left: 0px;
+}
+#breadcrumbs ul .current {
+	color: red;
+}
+#breadcrumbs ul li:before {
+	content: " > ";
+}
+#breadcrumbs ul li:first-child:before {
+	content: none;
+}
+</style>
---
+++ frontend/vue-project/src/views/AppTop.vue
@@ -0,0 +1,18 @@
+<!-- <script setup lang="ts">
+import {RouterLink} from "vue-router";
+</script> -->
+
+
+<template>
+    <h1>TOP</h1>
+    <nav id="breadcrumbs">
+        <ul>
+            <li>
+                <RouterLink to="/">TOP</RouterLink>
+            </li>
+        </ul>
+    </nav>
+    <section>
+        <p>会員管理はこちら</p>
+    </section>
+</template>
