c400918a977e8aaeb48fbd56387e2bbecbc360a7
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-23 11:33:59 +0900

    動的コンポーネント

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,62 +1,40 @@
 <script setup lang="ts">
-import { reactive, provide, ref } from 'vue';
-import BaseSection from './components/BaseSection.vue';
-import OneSection from './components/OneSection.vue';
-import TwoSection from './components/TwoSection.vue';
-import type { Member } from './stores/interfaces';
+import {ref} from "vue";
+import Input from "./components/Input.vue";
+import Radio from "./components/Radio.vue";
+import Select from "./components/Select.vue";
 
 
-const memberLists = reactive(
-	new Map<number, Member>([
-		[1, { id: 1, name: "田中", email: "tanaka@example.com", points: 35, note: "初回特典あり" }],
-		[2, { id: 2, name: "鈴木", email: "suzuki@example.com", points: 53 }],
-		[3, { id: 3, name: "伊藤", email: "ito@example.com", points: 80 }],
-	])
-);
-provide("memberLists", memberLists);
+const currentComponent = ref(Input);
+const currentComponentName = ref("Input");
+const componentList = [
+	Input,
+	Radio,
+	Select,
+];
+const componentNameList: string[] = [
+	"Input",
+	"Radio",
+	"Select",
+];
 
 
-const tanaka = ref("田中太郎");
-const tanakaProblemLists: string[] = ["電話が通じません", "留守です"];
-const tanakaProblems = ref(tanakaProblemLists);
-const suzuki = ref("鈴木次郎");
+let currentComponentIndex = 0;
+const switchComponent = (): void => {
+	currentComponentIndex++;
+	if (currentComponentIndex >= componentList.length) {
+		currentComponentIndex = 0;
+	}
+	currentComponent.value = componentList[currentComponentIndex]!;
+	currentComponentName.value = componentNameList[currentComponentIndex]!;
+};
 </script>
 
 
 <template>
-	<section>
-		<h2>Slotの利用</h2>
-		<OneSection v-bind:name="tanaka">
-			<template v-slot:default>
-				<p>以下の問題があります</p>
-			</template>
-			<template v-slot:detail>
-				<ul>
-					<li v-for="tanakaProblem in tanakaProblems" v-bind:key="tanakaProblem">
-						{{tanakaProblem}}
-					</li>
-				</ul>
-			</template>
-		</OneSection>
-		<OneSection v-bind:name="suzuki"></OneSection>
-	</section>
-
-
-	<section>
-		<TwoSection>
-			<template v-slot:default="slotProps">
-				<dl>
-					<dt>名前</dt>
-					<dd>{{slotProps.tanakaInfo.name}}</dd>
-					<dt>状況</dt>
-					<dd>{{slotProps.tanakaInfo.state}}</dd>
-				</dl>
-			</template>
-		</TwoSection>
-	</section>
-
-	<section>
-		<h2>Provide / Injectの利用</h2>
-		<BaseSection />
-	</section>
+	<p>コンポーネント名：{{ currentComponentName }}</p>
+	<keep-alive>
+		<component v-bind:is="currentComponent" />
+	</keep-alive>
+	<button v-on:click="switchComponent">コンポーネント切り替え</button>
 </template>
