09ed094d6d544ec35865030fdaae89c544800061
sugiura hiroki <iamsugihiro8180@gmail.com>
2025-10-24 16:18:47 +0900

    Storeの使い方

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,59 +1,32 @@
 <script setup lang="ts">
-// commit test
-import { reactive, provide } from "vue";
-import {RouterView} from "vue-router";
-import type { Member } from '@/stores/interfaces';
+import { computed } from "vue";
+import { useCounterStore } from "@/stores/counter";
 
 
-const memberLists = reactive(
-	new Map<number, Member>([
-		[33456, { id: 33456, name: "田中", email: "tanaka@example.com", points: 35, note: "初回特典あり" }],
-		[47783, { id: 47783, name: "鈴木", email: "suzuki@example.com", points: 53 }],
-		[53625, { id: 53625, name: "伊藤", email: "ito@example.com", points: 80 }],
-	])
+const counterStore = useCounterStore();
+const count = computed(
+	(): number => {
+		return counterStore.counter;
+	}
 );
-provide("memberLists", memberLists);
+
+
+const doubleCount = computed(
+	(): number => {
+		return counterStore.doubleCount;
+	}
+);
+
+
+const onIncrimentClick = () => {
+	counterStore.incrementCount();
+};
 </script>
 
 
+
 <template>
-	<header>
-		<h1>Vue Router サンプル</h1>
-	</header>
-	<main>
-		<RouterView />
-	</main>
+	<p>現在のポイント：{{ count }}</p>
+	<p>現在のポイントx2：{{ doubleCount }}</p>
+	<button v-on:click="onIncrimentClick">加算</button>
 </template>
-
-
-<style>
-main {
-	border: blue 1px solid;
-	padding: 10px;
-}
-
-#breadcrumbs ul li {
-	display: inline;
-	list-style-type: none;
-}
-
-#breadcrumbs {
-	margin-left: 0px;
-}
-
-#breadcrumbs ul {
-	padding-left: 0px;
-}
-
-#breadcrumbs ul .current {
-	color: red;
-}
-
-#breadcrumbs ul li:before {
-	content: " > ";
-}
-
-#breadcrumbs ul li:first-child:before {
-	content: none;
-}
-</style>
--- frontend/vue-project/src/stores/counter.ts
+++ frontend/vue-project/src/stores/counter.ts
@@ -1,12 +1,28 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
 
-  return { count, doubleCount, increment }
-})
+interface State {
+    counter: number;
+};
+
+
+export const useCounterStore = defineStore("counter",{
+    // id: "counter",
+    state: (): State => {
+        return {
+            counter: 0
+        };
+    },
+
+    getters: {
+        doubleCount: (state): number => {
+            return state.counter * 2;
+        }
+    },
+    
+    actions: {
+        incrementCount(): void {
+            this.counter++;
+        }
+    }
+});
